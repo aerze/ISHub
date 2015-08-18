776046496edd6b585e63ef159ca65c7fb6b996d3
Gus Suarez <gus@mythril.co>
2015-08-18 01:16:38 -0700

    change to materialize css, create SideNav and hook to router, display stored JSON

--- clientTool/lib/js/main.js
+++ clientTool/lib/js/main.js
@@ -4,30 +4,72 @@ $(document).on('ready', function () {
 
     var fileChecker = require('./lib/js/fileChecker');
     fileChecker.init();
+    window.LOCAL_ITEM_SETS = fileChecker.getItemSets();
     
-    var LOCAL_ITEM_SETS = fileChecker.getItemSets();
-    var ITEMSET_ID_COUNTER = 0;
-    window.LOCAL_ITEM_SETS = LOCAL_ITEM_SETS;
-    
-
-    UI.menu.removeLoading();
-
-    _.each(LOCAL_ITEM_SETS.global, function (itemSet, fileName) {
-        itemSet.ISHUB_ID = ITEMSET_ID_COUNTER++;
-        UI.menu.appendItem('global', itemSet.title, itemSet.ISHUB_ID);
-    });
-
-    _.each(LOCAL_ITEM_SETS.champs, function (champ, champName) {
-        UI.menu.appendGroup(champName);
-        _.each(champ, function (itemSet, fileName) {
-            itemSet.ISHUB_ID = ITEMSET_ID_COUNTER++;
-            UI.menu.appendItem(champName, itemSet.title, itemSet.ISHUB_ID);
-        });
-    });
-
-    $('a.pure-menu-link').on('click', function(e) {
-        e.preventDefault();
-
-        UI.main.show(LOCAL_ITEM_SETS.all[this.id]);
-    });
 });
+
+
+var fish = {};
+
+fish.router = function(route) {
+    console.log('fish::router: ' + route);
+    if (route === 'makeNewItemSet') {
+        $('.button-collapse').sideNav('hide');
+
+    } else if (route === 'makeNewItemSet') {
+        $('.button-collapse').sideNav('hide');
+
+    } else if (route === 'importItemSet') {
+        $('.button-collapse').sideNav('hide');
+    } else if (route.indexOf('/') > -1) {
+        $('.button-collapse').sideNav('hide');
+
+        var _id = parseInt(route.split(':')[1], 10);
+        console.log(_id);
+        var all = LOCAL_ITEM_SETS.all;
+        for (var i = all.length - 1; i >= 0; i--) {
+            console.log(all[i]._id, _id);
+            if (all[i]._id === _id) {
+                console.log(all[i]);
+                UI.display(all[i]);
+                break;
+            }
+        }
+    } else {
+        UI.nav.setList(route);
+    }
+
+
+};
+
+fish.file = {
+    getGlobalItemSets: function () {
+        var results = [];
+        for (var i = 0; i < LOCAL_ITEM_SETS.all.length; i++) {
+            var itemSet = LOCAL_ITEM_SETS.all[i];
+            if (itemSet.isGlobalForChampions) {
+                results.push(itemSet.title + ' ID:' + itemSet._id);
+            }
+        }
+        return results;
+    },
+    getChampionsWithSets: function () {
+        var results = [];
+        if (LOCAL_ITEM_SETS.champs) {
+            for (var name in LOCAL_ITEM_SETS.champs) {
+                results.push(name);
+            }
+        }
+        return results;
+    },
+    getChampionItemSets: function (champion) {
+        var results = [];
+        if (LOCAL_ITEM_SETS.champs[champion]) {
+            for (var key in LOCAL_ITEM_SETS.champs[champion]) {
+                var itemSet = LOCAL_ITEM_SETS.champs[champion][key];
+                results.push(itemSet.title + ' ID:' + itemSet._id);
+            }
+        }
+        return results;
+    }
+};
--- clientTool/lib/js/ui.js
+++ clientTool/lib/js/ui.js
@@ -48,7 +48,7 @@ UI.buttons.min.onclick = function () {
     win.minimize();
 };
 
-
+// OLD CODE FROM PURE MENU
 UI.menu = {
     globalSets: $('#globalSets'),
     champSets: $('#champSets'),
@@ -84,5 +84,126 @@ UI.main = {
     show: function (itemSet) {
         var code = $('<code>' + JSON.stringify(itemSet, null, '\t').replace(/\n/g, '<br/>') +'</code>');
         this.main.html(code);
+    }
+};
+
+UI.display = function (itemSet) {
+    var code = $('<code>' + JSON.stringify(itemSet, null, '\t').replace(/\n/g, '<br/>') +'</code>');
+    $('.content').html(code);
+    $('.content').addClass('json');
+    console.log(itemSet);
+};
+
+UI.nav = {
+    LISTS: {
+        main: [
+            'My Item Sets',
+            'Make New Item Set',
+            'Import Item Set',
+            'Export Item Set'
+        ],
+        myItemSets: [
+            'Back',
+            'Global',
+            'Champion'
+        ],
+        myItemSetsGlobal: function () {
+            return fish.file.getGlobalItemSets();
+        },
+        myItemSetsChampions: function () {
+            return fish.file.getChampionsWithSets();
+        },
+        myItemSetsChampionItemsSets: function (champion) {
+            return fish.file.getChampionItemSets(champion);
+        }
+
+    },
+
+
+
+    setList: function (route) {
+        console.log('UI::nav: Setting nav to ' + route);
+        var listItems = [],
+            list = this.LISTS[route];
+
+        if (!Array.isArray(list)) {
+            if (route.indexOf('#champion-') > -1) {
+                list = this.LISTS.myItemSetsChampionItemsSets(route.split('-')[1]);
+            } else {
+                if (list === undefined) return;
+                list = list(route);
+            }
+            list.unshift('back');
+        }
+
+        listItems = this.renderList(list);
+
+
+        if (route === 'main') {
+            listItems[0].on('click', function () {  fish.router('myItemSets');  });
+            listItems[1].on('click', function () {  fish.router('makeNewItemSet');  });
+            listItems[2].on('click', function () {  fish.router('importItemSet');  });
+            listItems[3].on('click', function () {  fish.router('exportItemSet');  });
+
+        } else if (route === 'myItemSets') {
+            listItems[0].on('click', function () {  fish.router('main');  });
+            listItems[1].on('click', function () {  fish.router('myItemSetsGlobal');  });
+            listItems[2].on('click', function () {  fish.router('myItemSetsChampions');  });
+
+        } else if (route === 'myItemSetsGlobal') {
+
+            listItems[0].on('click', function () {  fish.router('myItemSets');  });
+            var globalItemSetClick = function () { 
+                var itemSetName = $(this).find('a').html();
+                var newRoute = route + '#global/' + itemSetName;
+                fish.router(newRoute);       
+            };
+
+            for (var i = 1; i < listItems.length; i++) {
+                listItems[i].on('click', globalItemSetClick);
+            }
+
+        } else if (route === 'myItemSetsChampions') {
+            listItems[0].on('click', function () {  fish.router('myItemSets');  });
+            var championClick = function () { 
+                var championName = $(this).find('a').html();
+                var newRoute = route + '#champion-' + championName;
+                fish.router(newRoute);
+            };
+
+            for (var i = 1; i < listItems.length; i++) {
+                listItems[i].on('click', championClick);
+            }
+        } else if (route.indexOf('#champion-') > -1) {
+            listItems[0].on('click', function () {  fish.router('myItemSetsChampions');  });
+            var championItemSetClick = function () {
+                var itemSetName = $(this).find('a').html();
+                var newRoute = route + '/' + itemSetName;
+                fish.router(newRoute);
+            };
+
+            for (var i = 1; i < listItems.length; i++) {
+                listItems[i].on('click', championItemSetClick);
+            }
+        }
+    },
+
+    
+    renderList: function (nameArray) {
+        var sideNav = $('#slide-out'),
+            listItems = [],
+            item = {};
+            
+        sideNav.html('');
+        for (var i = 0; i < nameArray.length; i++) {
+            item = $('<li><a href="#!">' + nameArray[i] + '</a></li>');
+            sideNav.append(item);
+            listItems.push(item);
+        }
+        return listItems;
+    },
+
+    init: function () {
+
     }
 };
